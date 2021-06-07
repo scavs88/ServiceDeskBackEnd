1239a71b3411c9ff1181592ac65c2b7120d3c4de
Frednel Jean-Joseph <mr.jeanjoseph@gmail.com>
2021-06-07 14:11:42 -0400

    working database

--- ServiceDesk/Controllers/BookMarkedController.cs
+++ ServiceDesk/Controllers/BookMarkedController.cs
@@ -21,15 +21,31 @@ namespace ServiceDesk.Controllers
             }
         }
 
+
+
+        [HttpGet("bookmarkedBy")]
+        public List<BookMarked> GetAllByUserId(string bookmarkedBy)
+        {
+            using (ServiceDeskDBContext context = new ServiceDeskDBContext())
+            {
+                List<BookMarked> list = new List<BookMarked>();
+
+                list = context.BookMarkeds.Where(b => b.BookMarkedBy.ToLower() == bookmarkedBy.ToLower()).ToList();
+                return list;
+            }
+        }
+
+
+
         [HttpPost]
-        public BookMarked AddBookMarked(string bookmarkedby, int id)
+        public BookMarked AddBookMarked(string bookmarkedby, int ticketId)
         {
             using (ServiceDeskDBContext context = new ServiceDeskDBContext())
             {
                 BookMarked b = new BookMarked()
                 {
                     BookMarkedBy = bookmarkedby,
-                    Id = id
+                    TicketId = ticketId
                 };
                 context.BookMarkeds.Add(b);
                 context.SaveChanges();
--- ServiceDesk/Controllers/ResolutionController.cs
+++ ServiceDesk/Controllers/ResolutionController.cs
@@ -1,41 +1,41 @@
-﻿//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Mvc;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-//namespace ServiceDesk.Controllers
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class ResolutionController : ControllerBase
-//    {
-//        [HttpGet()]
-//        public List<Resolution> GetResolutions()
-//        {
-//            using (ServiceDeskDBContext context = new ServiceDeskDBContext())
-//            {
-//                return context.Resolutions.ToList();
-//            }
-//        }
+namespace ServiceDesk.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResolutionController : ControllerBase
+    {
+        [HttpGet()]
+        public List<Resolution> GetResolutions()
+        {
+            using (ServiceDeskDBContext context = new ServiceDeskDBContext())
+            {
+                return context.Resolutions.ToList();
+            }
+        }
 
-//        [HttpPost]
-//        public Resolution AddResultion(int id, string ticketname, string resolution, string closedby)
-//        {
-//            using (ServiceDeskDBContext context = new ServiceDeskDBContext())
-//            {
-//                Resolution r = new Resolution()
-//                {
-//                    Id = id,
-//                    TicketName = ticketname,
-//                    Resolution1 = resolution,
-//                    ClosedBy = closedby
-//                };
-//                context.Resolutions.Add(r);
-//                context.SaveChanges();
-//                return r;
-//            }
-//        }
-//    }
-//}
+        [HttpPost]
+        public Resolution AddResultion(int id, string ticketname, string resolution, string closedby)
+        {
+            using (ServiceDeskDBContext context = new ServiceDeskDBContext())
+            {
+                Resolution r = new Resolution()
+                {
+                    Id = id,
+                    TicketName = ticketname,
+                    Resolution1 = resolution,
+                    ClosedBy = closedby
+                };
+                context.Resolutions.Add(r);
+                context.SaveChanges();
+                return r;
+            }
+        }
+    }
+}
